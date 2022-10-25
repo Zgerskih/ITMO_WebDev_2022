79354f57d71a08a9f93ecc59a32835d6b84cb9f2
Zgerskih <114507567+Zgerskih@users.noreply.github.com>
2022-10-25 16:17:55 +0300

    add class

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const domInput = document.getElementById('inpTodoTitle');
 const domBtnCreateTodo = document.getElementById('btnCreateTodo');
 const domListOfTodos = document.getElementById('listOfTodos');
+const domTotal= document.getElementById('total')
+// add event
 
 domBtnCreateTodo.addEventListener("click", (e)=>{
 console.log(domInput.value)
@@ -8,14 +10,20 @@ console.log(domInput.value)
   if (domInput.value === '')return
   createDeleteElements(domInput.value)
   domInput.value = ''
-
 })
 
 function createDeleteElements(value) {
   console.log(value)
+
   const li =document.createElement('li')
-  li.className = 'li'
+  const btn =document.createElement('button')
+
+    li.className = 'li'
   li.textContent = value
+
+  btn.className = 'btn'
+  btn.textContent = 'add'
+li.appendChild(btn)
 
   domListOfTodos.appendChild(li)
 }
