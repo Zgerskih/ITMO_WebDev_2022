5e8fcc812e1d5af4b5e206c433b4f4225d66f511
art <arturzgerskih1@gmail.com>
2022-10-25 18:53:52 +0300

    car color

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-const domInput = document.getElementById('inpTodoTitle');
-const domBtnCreateTodo = document.getElementById('btnCreateTodo');
-const domListOfTodos = document.getElementById('listOfTodos');
 
-domBtnCreateTodo.addEventListener("click", (e)=>{
-console.log(domInput.value)
 
-  if (domInput.value === '')return
-  createDeleteElements(domInput.value)
-  domInput.value = ''
 
-})
-
-function createDeleteElements(value) {
-  console.log(value)
-  const li =document.createElement('li')
-  li.className = 'li'
-  li.textContent = value
-
-  domListOfTodos.appendChild(li)
+class IHaveWehicle {
+  constructor(color, speed, weight, price) {
+    this.color = color;
+    this.speed = speed
+    this.weight= weight
+    this.price= price
+  }
 }
+let car = new IHaveWehicle("red", 250, 1200, "1200$" )
+
+let bicicle = new IHaveWehicle("yellow", 40, 30, "25$")
+
+function changeColor(inputWehicle, inputcolor) {
+  if(inputWehicle.hasOwnProperty("color")){
+    inputWehicle.color = inputcolor;
+  }
+}
+
+console.log("car befor: ", car.color, car.speed, car.weight, car.price);
+changeColor(car, "red")
+console.log("car after: ", car.color);
+
+
+console.log("bicicle befor: ", bicicle.color, bicicle.speed, bicicle.weight, bicicle.price);
+changeColor(bicicle, "blue")
+console.log("bicicle after: ", bicicle.color);
+
