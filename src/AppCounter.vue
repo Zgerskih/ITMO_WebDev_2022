ab60c5cfd6b077471827bd4e758eb9ae2a86b795
art <arturzgerskih1@gmail.com>
2022-11-26 13:29:08 +0300

    less 27

---
+++ src/AppCounter.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1>App Counter</h1>
+  <p v-html='innerHTML'></p>
+  <CounterValue v-for="obj in [  {index:1, text: 'Clicked', counter: counter}]"
+  :title="obj.text"
+  :value="counter"
+  :key="obj.index"
+  />
+
+  <button v-on:click="onPlus($event, 1)">+</button>
+  <button v-if="canRenderMinusButton > 0" @click="onMinus">-</button>
+</template>
+<script>
+
+import CounterValue from '@/components/CounterValue.vue';
+export default {
+  components: { CounterValue },
+  data() {
+    return {
+     counter: 0,
+      innerHTML: `<h2>Hello</h2>`
+              };
+          },
+          computed: {
+    canRenderMinusButton(){
+      return this.counter > -10;
+     },
+       },
+          methods: {
+            onPlus(event, index){
+              this.counter++;
+              console.log('>counter -> onPlus', this.counter, event, index);
+        },
+            onMinus(){
+              this.counter--;
+              console.log('>counter -> onMinus', this.counter);
+        },
+    }
+}
+
+</script>>
+<style scoped>
+p.counter-value{
+  color: rebeccapurple;
+  font-size: 2rem;
+  font-weight: normal;
+}
+</style>
---
+++ src/components/CounterValue.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <p class='value'>
+    <span style='color: #646cff'>{{ title }} </span>: {{ value }}
+  </p>
+
+</template>
+<script>
+export default {
+  props:{
+    title: "",
+    value: 1,
+  },
+};
+</script>>
+<style scoped>
+p.counter-value{
+  color: rebeccapurple;
+  font-size: 2rem;
+  font-weight: normal;
+}
+</style>
