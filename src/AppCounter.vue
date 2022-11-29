6d477993a78c23ca410dd34651d7a929c7a58c87
art <arturzgerskih1@gmail.com>
2022-11-29 22:05:31 +0300

    end lesson

--- src/AppCounter.vue
+++ src/AppCounter.vue
@@ -1,48 +1,82 @@
 <template>
-  <h1>App Counter</h1>
-  <p v-html='innerHTML'></p>
-  <CounterValue v-for="obj in [  {index:1, text: 'Clicked', counter: counter}]"
-  :title="obj.text"
-  :value="counter"
-  :key="obj.index"
-  />
+  <h1 ref="Header">App Counter</h1>
+  <button v-on:click="ADD">кнопка жмяк</button>
+  <input v-model="message" placeholder="editMe" />
 
+  <p v-html="innerHTML"></p>
+  <CounterValue
+    class="counter"
+    v-for="obj in [{ index: 1, text: 'Clicked', counter: counter }]"
+    :title="obj.text"
+    :value="counter"
+    :key="obj.index"
+  />
   <button v-on:click="onPlus($event, 1)">+</button>
   <button v-if="canRenderMinusButton > 0" @click="onMinus">-</button>
 </template>
 <script>
+import CounterValue from './components/CounterValue.vue';
 
-import CounterValue from '@/components/CounterValue.vue';
+const LOCAL_KEY_COUNTER = 'counter';
+const saveCounter = (value) => localStorage.setItem(LOCAL_KEY_COUNTER, value);
+let counterWatcher = null;
+
 export default {
   components: { CounterValue },
   data() {
     return {
-     counter: 0,
-      innerHTML: `<h2>Hello</h2>`
-              };
-          },
-          computed: {
-    canRenderMinusButton(){
-      return this.counter > -10;
-     },
-       },
-          methods: {
-            onPlus(event, index){
-              this.counter++;
-              console.log('>counter -> onPlus', this.counter, event, index);
-        },
-            onMinus(){
-              this.counter--;
-              console.log('>counter -> onMinus', this.counter);
-        },
-    }
-}
+      counter: 0,
+    };
+  },
+  created() {
+    console.log('> created:', this.counter);
+    this.counter = localStorage.getItem(LOCAL_KEY_COUNTER) || 0;
+    counterWatcher = this.$watch(
+      () => this.counter,
+      (newValue, oldValue) => {
+        console.log('> counter watched:', { newValue, oldValue });
+        saveCounter(newValue);
+      },
+    );
+  },
+  mounted() {
+    console.log('> created: ', this.counter);
+  },
+  unmounted() {
+    counterWatcher();
+    console.log('> created: ', this.counter);
+  },
 
-</script>>
-<style scoped>
-p.counter-value{
-  color: rebeccapurple;
-  font-size: 2rem;
-  font-weight: normal;
+  computed: {
+    canRenderMinusButton() {
+      return this.counter > -10;
+    },
+  },
+
+  methods: {
+    onPlus(event, index) {
+      this.counter++;
+      if (this.counter > 13) this.$destroy();
+      saveCounter(this.counter);
+      console.log('>counter -> onPlus', this.counter, this);
+    },
+    onMinus() {
+      this.counter--;
+      if (this.counter === 0) {
+        this.$refs.header.innerHTML = `<b>Header:</b>;`;
+        saveCounter(this.counter);
+      }
+      console.log('>counter -> onMinus', this.counter);
+    },
+    editMe(e) {
+      this.values();
+      console.log('values -> editMe', this.values);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.counter {
+  color: green;
 }
 </style>
--- src/components/CounterValue.vue
+++ src/components/CounterValue.vue
@@ -1,22 +1,28 @@
 <template>
-
-  <p class='value'>
-    <span style='color: #646cff'>{{ title }} </span>: {{ value }}
+  <p class="value" :class="{ alert: isCounterAlert }">
+    <span style="color: #646cff">{{ title }} </span>: {{ value }}
   </p>
-
 </template>
 <script>
 export default {
-  props:{
-    title: "",
-    value: 1,
+  props: {
+    title: '',
+    value: 0,
+  },
+  computed: {
+    isCounterAlert() {
+      return this.value >= 10;
+    },
   },
 };
-</script>>
-<style scoped>
-p.counter-value{
+</script>
+<style lang="scss" scoped>
+p.counter-value {
   color: rebeccapurple;
   font-size: 2rem;
   font-weight: normal;
+}
+.alert {
+  background: red;
 }
 </style>
