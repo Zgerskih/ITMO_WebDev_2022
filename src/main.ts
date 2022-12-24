23f727da5a45b77bc6f0fe65a13a6f6f2f8b27b4
art <arturzgerskih1@gmail.com>
2022-12-24 10:52:15 +0300

    pocket base

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-const db = new PouchDB('http://localhost:5984/books', {skip_setup: true});
 const app = createApp(App);
-app.provide('db', db);
-app.mount('#app');
+// app.provide('db', db);
+app.mount("#app");
